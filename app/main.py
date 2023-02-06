1997ea9fd3b0cc3999931e2860ed0b0fb613abf7
Oluwaseun <tanimolaoluwaseun70@gmail.com>
2023-02-06 18:45:49 +0100

    error handling

--- app/main.py
+++ app/main.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI, Depends, status, Response
+from fastapi import FastAPI, Depends, status, HTTPException
 from . import schemas, models
 from .database import SessionLocal, engine
 from sqlalchemy.orm import Session
@@ -15,17 +15,22 @@ def get_db():
     finally:
         db.close()
 
+
 @app.get("/blog", status_code=status.HTTP_200_OK)
 def show_all(db: Session = Depends(get_db)):
     notes = db.query(models.Note).all()
     return notes
 
+
 @app.get("/blog{id}", status_code=status.HTTP_200_OK)
-def note(id, response: Response, db: Session = Depends(get_db)):
+def note(id, db: Session = Depends(get_db)):
     note = db.query(models.Note).filter(models.Note.id == id).first()
     if not note:
-        response.status_code=status.HTTP_404_NOT_FOUND
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail = f"Note with id {id} is not available")
+
+
     return note
+
 
 @app.post("/blog", status_code=status.HTTP_201_CREATED)
 def create_notes(request: schemas.Note, db: Session = Depends(get_db)):
