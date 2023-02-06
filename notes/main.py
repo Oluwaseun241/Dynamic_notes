dd179b4f462a89b56f4546e294d96dd488c5e90b
Oluwaseun <tanimolaoluwaseun70@gmail.com>
2023-02-06 02:23:52 +0100

    database: sucessful

--- notes/main.py
+++ notes/main.py
@@ -1,14 +1,28 @@
-from fastapi import FastAPI
-from . import schemas
+from fastapi import FastAPI, Depends
+from . import schemas, models
+from .database import SessionLocal, engine
+from sqlalchemy.orm import Session
 
+
+models.Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
 
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
 
-@app.get("/")
-def notes():
-    return 'all'
+# @app.get("/")
+# def notes():
+#     return 'all'
 
 @app.post("/blog")
-def create_notes(request: schemas.Note):
-    return f'Blog created {request.title}'
+def create_notes(request: schemas.Note, db: Session = Depends(get_db)):
+    new_note = models.Note(title=request.title, body=request.body)
+    db.add(new_note)
+    db.commit()
+    db.refresh(new_note)
+    return new_note
--- notes/schemas.py
+++ notes/schemas.py
@@ -1,6 +1,5 @@
 from pydantic import BaseModel
 
 class Note(BaseModel):
-    # id: int
     title: str
     body: str
