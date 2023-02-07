801ee1a618c4a4587002f72e4c97a97aa1e429b8
Oluwaseun <tanimolaoluwaseun70@gmail.com>
2023-02-07 13:19:18 +0100

    omo

--- app/main.py
+++ app/main.py
@@ -40,6 +40,7 @@ def create_notes(request: schemas.Note, db: Session = Depends(get_db)):
     db.refresh(new_note)
     return new_note
 
+
 @app.delete("/note{id}", status_code=status.HTTP_202_ACCEPTED)
 def delete_note(id, db: Session = Depends(get_db)):
     note = db.query(models.Note).filter(models.Note.id == id).delete(synchronize_session=False)
@@ -51,3 +52,14 @@ def delete_note(id, db: Session = Depends(get_db)):
 
     return {"detail": f"Note with id {id} is sucessfully deleted"}
     
+
+@app.put("/note{id}", status_code=status.HTTP_200_OK)
+def update_note(id, request: schemas.Note, db: Session = Depends(get_db)):
+    note = db.query(models.Note).filter(models.Note.id == id)
+
+    if not note.first():
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail = f"Note with id {id} is not available")
+    
+    note.update(request, synchronize_session=False)
+    db.commit()
+    return {"detail": f"Note with id {id} is sucessfully updated"}
