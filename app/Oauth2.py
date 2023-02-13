be272ce89be169d9ef7a6f93bee276c8fcf44375
Oluwaseun <tanimolaoluwaseun70@gmail.com>
2023-02-14 00:29:21 +0100

    auth: routes

--- app/Oauth2.py
+++ app/Oauth2.py
@@ -1,13 +1,13 @@
 from fastapi import Depends, FastAPI, HTTPException, status
-from fastapi.security import OAuth2PasswordBearer
+from fastapi.security import OAuth2PasswordBearer, OAuth2PasswordRequestForm
 from .import token
 
 oauth2_scheme = OAuth2PasswordBearer(tokenUrl="login")
 
-async def get_current_user(token: str = Depends(oauth2_scheme)):
+async def get_current_user(data: str = Depends(oauth2_scheme)):
     credentials_exception = HTTPException(
         status_code=status.HTTP_401_UNAUTHORIZED,
         detail="Could not validate credentials",
         headers={"WWW-Authenticate": "Bearer"},
     )
-    return token.verify_token(token, credentials_exception)
+    return token.verify_token(data, credentials_exception)
--- app/routers/auth.py
+++ app/routers/auth.py
@@ -2,13 +2,14 @@ from fastapi import APIRouter, Depends, HTTPException, status
 from .. import schemas, models, database, token
 from ..hash import Hash
 from sqlalchemy.orm import Session
+from fastapi.security import OAuth2PasswordRequestForm
 
 router = APIRouter(
     tags=["Authentication"]
 )
 
 @router.post("/login")
-def login(request: schemas.Login, db: Session = Depends(database.get_db)):
+def login(request: OAuth2PasswordRequestForm = Depends(), db: Session = Depends(database.get_db)):
     user = db.query(models.User).filter(models.User.username == request.username).first()
     if not user:
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"Invalid Credentials")
